553658096da3e1b20859eaacfd86b20c3db8843c
rtmkibish <rtmkibish@gmail.com>
2020-09-07 20:22:09 +0300

    final

--- script.js
+++ script.js
@@ -1,58 +1,69 @@
-class MilitaryResource {
-    constructor(type, health, distance) {
-        this.type = type;
-        this.health = health;
-        this.maxHealth = 100;
-        this.distance = distance;
-        this.maxDistance = 1000;
-    }
-    isReadyToMove() {
-        console.log('Ready to move');
-    }
-    isReadyToFight() {
-        console.log('Ready to fight');
-    }
-    restore() {
-        this.health = this.maxHealth;
-    };
-    clone() {
-        return new MilitaryResource(this.type, this.health, this.distance);
-    };
- }
+// class MilitaryResource {
+//     constructor(type, health, distance) {
+//         this.type = type;
+//         this.health = health;
+//         this.maxHealth = 100;
+//         this.distance = distance;
+//         this.maxDistance = 1000;
+//     }
+//     isReadyToMove() {
+//         console.log('Ready to move');
+//     }
+//     isReadyToFight() {
+//         console.log('Ready to fight');
+//     }
+//     restore() {
+//         this.health = this.maxHealth;
+//     };
+//     clone() {
+//         return new MilitaryResource(this.type, this.health, this.distance);
+//     };
+//  }
  
-const first = new MilitaryResource("infantryman", 80, 100);
-console.log(first);
-
-// first.restore();
+// const first = new MilitaryResource("infantryman", 80, 100);
 // console.log(first);
 
+// // first.restore();
+// // console.log(first);
 
-const first2 = first.clone();
-first2.restore();
-console.log(first2);
+
+// const first2 = first.clone();
+// first2.restore();
+// console.log(first2);
  
  
-class Squad extends MilitaryResource {
-    constructor(defaultResources){
-        this.squad = [];
-        if (defaultResources) this.combineResources(defaultResources);
-    }
-    restoreAll() {
-        this.forEach(element => {
-            super.restore();
-        });
-    };
-    getReadyToMoveResources() {
-        super.isReadyToMove();
-    };
-    combineResources() {
-        this.push();
-    };
-    // cloneResource(resourseClone) {
-    //     let resourseClone = new Squad
-    // };
- }
+// class Squad extends MilitaryResource {
+//     constructor(defaultResources){
+//         super();
+//         this.squad = [];
+//         if (defaultResources) this.combineResources(defaultResources);
+//     }
+//     restoreAll() {
+//         this.forEach(element => {
+//             this.restore();
+//         });
+//     };
+//     getReadyToMoveResources() {
+//         this.isReadyToMove();
+//     };
+//     combineResources(a) {
+//         this.squad.push(a);
+//     };
+//     // cloneResource(resourseClone) {
+//     //     let resourseClone = new Squad
+//     // };
+//  }
  
- const party = new Squad(first);
- party.combineResources(first2);
- console.log(party);
+//  const party = new Squad(first);
+// //  party.combineResources(first2);
+//  console.log(party);
+
+const units = [new Unit('Virus', 200, 500)];
+units.push(units[0].clone());
+
+const squad = new Squad(units);
+console.log(squad);
+
+
+
+
